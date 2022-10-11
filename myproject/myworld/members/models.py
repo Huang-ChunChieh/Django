67a6c5e7ec62ed3920a93e3a04178a3a0881b3c5
Huang-ChunChieh <jay24585978@gmail.com>
2022-10-11 21:01:22 +0800

    Finish Day01-09(在Python Shell新增Records)

--- myproject/myworld/members/models.py
+++ myproject/myworld/members/models.py
@@ -1,3 +1,11 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+
+class Members(models.Model):  # 表示Members繼承自models.Model，也就是Members是一個名為Members的model的意思
+    firstname = models.CharField(max_length=255)
+    # 為資料表建立名為firstname的字串欄位並限制字串最大長度為255
+    lastname = models.CharField(max_length=255)
+    # 為資料表建立名為lastname的字串欄位並限制字串最大長度為255
--- myproject/myworld/members/views.py
+++ myproject/myworld/members/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+#from re import template
+#from unittest import loader#
+#from django.shortcuts import render
 from django.http import HttpResponse  # import 需要的套件
+from django.template import loader  # 用來載入template的套件
+from .models import Members  # 把Members這張資料表 import 進來
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World")
-# 建立名為index的方法，回應訪問者的request，會回傳Hello World!到訪問者端(也就是顯示在訪問者開啟的網頁上)
+    mymembers = Members.objects.all().values()
+    # 建立變數mymembers存放Members上的所有紀錄，這些紀錄會被包在一個List裡面被回傳
+    output = ""  # output
+    for x in mymembers:  # 使用For迴圈逐一將Members上每筆紀錄的firstname欄位值和output串接在一起
+        output += x["firstname"]
+    return HttpResponse(output)  # 將這些字串回傳到瀏覽器並顯示在畫面上
