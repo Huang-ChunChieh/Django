751d9961df4a6d3ee585efeeba2d4ded7dbfc71a
Huang-ChunChieh <jay24585978@gmail.com>
2022-10-13 19:56:39 +0800

    add day12 txt file

--- myproject/myworld/members/models.py
+++ myproject/myworld/members/models.py
@@ -9,3 +9,10 @@ class Members(models.Model):  # 表示Members繼承自models.Model，也就是Me
     # 為資料表建立名為firstname的字串欄位並限制字串最大長度為255
     lastname = models.CharField(max_length=255)
     # 為資料表建立名為lastname的字串欄位並限制字串最大長度為255
+
+
+class Members2(models.Model):  # 表示Members繼承自models.Model，也就是Members是一個名為Members的model的意思
+    firstname = models.CharField(max_length=255)
+    # 為資料表建立名為firstname的字串欄位並限制字串最大長度為255
+    lastname = models.CharField(max_length=255)
+    # 為資料表建立名為lastname的字串欄位並限制字串最大長度為255
